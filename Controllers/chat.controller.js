c6b247bbc8d16331b65a4f7026b20041ef2c6f3f
junrari <xsx3362@naver.com>
2023-12-12 21:16:33 +0900

    채팅방 채팅 업데이트

--- Controllers/chat.controller.js
+++ Controllers/chat.controller.js
@@ -1,16 +1,24 @@
 const Chat = require("../Models/chat")
 const chatController ={}
 
-chatController.saveChat =async(message,user)=>{
-    const newMessage = new Chat({
-        chat: message,
-        user:{
-            id:user._id,  
-            name:user.name
-        }
+chatController.saveChat = async (message, user) => {
+    const newChat = new Chat({
+      chat: message,
+      user: {
+        id: user._id,
+        name: user.name,
+      },
+      room: user.room, // 메세지에 채팅방 정보도 저장하는 부분 추가! 
     });
-    await newMessage.save();   //save()은 몽고디비에서 데이터 삽입하는 메소드
-    return newMessage;
+    await newChat.save();
+    return newChat;
+  };
+
+chatController.showChat = async (roomId) =>{
+  const roomChat = await Chat.find({room:roomId})
+  if(!roomChat) throw new Error("chatting not found")
+  return roomChat;
 }
+
 
 module.exports=chatController
--- Models/user.js
+++ Models/user.js
@@ -13,5 +13,9 @@ const userSchema = new mongoose.Schema({
         type: Boolean,
         default: false,
     },
+    room:{
+        type: mongoose.Schema.ObjectId,
+        ref:"Room",
+    },
 });
 module.exports = mongoose.model("User",userSchema)
